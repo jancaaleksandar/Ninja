df3cdb1f9bea0cd037a1d4def0464d3c58629112
Aleksandar Janca <janca.aleksandar1@gmail.com>
2023-02-12 23:16:04 +0200

    deployment

--- pages/404.js
+++ pages/404.js
@@ -2,13 +2,14 @@ import Link from "next/link";
 import { useEffect } from "react";
 import { useRouter } from "next/router";
 
+
+
 const NotFound = () => {
-    const router = useRouter();
 
     useEffect(() => {
         setTimeout(() => {
             //router.go(-1)
-            router.push('/')
+            useRouter.push('/')
         }, 3000)
     }, [])
     return (
